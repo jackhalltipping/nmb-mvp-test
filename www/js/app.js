7d1ea35423539ebfb35edfd53471e0090c4949d1
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-08-12 17:03:17 +0100

    basic json get

---
+++ www/js/app.js
@@ -0,0 +1,58 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('noBullshit', ['ionic']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.controller('MainCtrl', function($scope, $http) {
+
+  $scope.restaurants = [];
+
+  $http.get('http://pacific-everglades-2252.herokuapp.com/api/jobseekers').then(function(resp) {
+      $scope.jobseekers = resp.data;
+      console.log($scope.jobseekers)
+  }, function(err) {
+    console.error('ERR', err); // err.status will contain the status code
+  });
+});
+
+
+app.controller('DashCtrl', function($scope, $ionicDeploy) {
+
+  // Update app code with new release from Ionic Deploy
+  $scope.doUpdate = function() {
+    $ionicDeploy.update().then(function(res) {
+      console.log('Ionic Deploy: Update Success! ', res);
+    }, function(err) {
+      console.log('Ionic Deploy: Update error! ', err);
+    }, function(prog) {
+      console.log('Ionic Deploy: Progress... ', prog);
+    });
+  };
+
+  // Check Ionic Deploy for new code
+  $scope.checkForUpdates = function() {
+    console.log('Ionic Deploy: Checking for updates');
+    $ionicDeploy.check().then(function(hasUpdate) {
+      console.log('Ionic Deploy: Update available: ' + hasUpdate);
+      $scope.hasUpdate = hasUpdate;
+    }, function(err) {
+      console.error('Ionic Deploy: Unable to check for updates', err);
+    });
+  }
+
+})
